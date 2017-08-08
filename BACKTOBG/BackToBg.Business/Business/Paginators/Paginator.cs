3e72b44774e430c125db50b6bfa3992a8ad44432
GenchoBG <genchobg11@gmail.com>
2017-08-08 12:48:51 +0300

    Made the paginator class generic so it's very reusable

--- BACKTOBG/BackToBg.Business/Business/Paginators/Paginator.cs
+++ BACKTOBG/BackToBg.Business/Business/Paginators/Paginator.cs
@@ -4,25 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BackToBg.Core.Business.UtilityInterfaces;
+using BackToBg.Core.Models.Utilities;
 
 namespace BackToBg.Core.Business.Paginators
 {
-    public abstract class Paginator : IPaginator
+    public abstract class Paginator<T> : IPaginator
     {
+        private const int ItemsPerPage = 10;
+        protected string name;
         protected int pageNumber;
         protected bool shouldBeRunning;
         protected IReader reader;
         protected IWriter writer;
+        protected int lineNumber;
 
-        public Paginator(IReader reader, IWriter writer)
+
+        public Paginator(IReader reader, IWriter writer, string name)
         {
             this.reader = reader;
             this.writer = writer;
+            this.name = name;
             this.pageNumber = 0;
+            this.lineNumber = 0;
             this.shouldBeRunning = true;
         }
 
-        public IList<string> Text;
+        protected abstract IReadOnlyList<T> Items { get; }
 
         public void Start()
         {
@@ -33,9 +40,45 @@ namespace BackToBg.Core.Business.Paginators
             }
         }
 
-        protected abstract int PagesCount { get; }
+        protected virtual int PagesCount
+        {
+            get
+            {
+                var rem = this.Items.Count % ItemsPerPage != 0 ? 1 : 0;
+                var result = this.Items.Count / ItemsPerPage + rem;
 
-        protected abstract void DisplayPages();
+                return result;
+            }
+        }
+
+        protected virtual void DisplayPages()
+        {
+            this.writer.Clear();
+            this.writer.SetCursorPosition((Constants.ConsoleWidth - this.name.Length) / 2, Constants.ConsoleHeight / 2 - 1);
+            this.writer.DisplayMessageInColor(this.name, ConsoleColor.Green);
+            var items = this.Items;
+            var questCount = items.Count;
+            var maxItemsOnCurrentPage = this.pageNumber * (this.PagesCount) + ItemsPerPage;
+            var upperBorder = Math.Min(maxItemsOnCurrentPage, questCount);
+            var index = this.pageNumber * ItemsPerPage;
+            for (; index < upperBorder; index++)
+            {
+                var itemAsString = this.RepresentItem(items[index]);
+                this.writer.SetCursorPosition((Constants.ConsoleWidth - itemAsString.Length) / 2, Constants.ConsoleHeight / 2 + index % ItemsPerPage);
+                if (index == this.lineNumber + this.pageNumber * ItemsPerPage)
+                {
+                    this.writer.WriteLine($">{itemAsString}<");
+                }
+                else
+                {
+                    this.writer.WriteLine($"{itemAsString}");
+                }
+            }
+
+            var message = $"Page {this.pageNumber + 1}/{this.PagesCount}";
+            this.writer.SetCursorPosition((Constants.ConsoleWidth - message.Length) / 2, Constants.ConsoleHeight / 2 + ItemsPerPage + 1);
+            this.writer.DisplayMessageInColor(message, ConsoleColor.Magenta);
+        }
 
         protected abstract void ExecuteAction();
 
@@ -53,6 +96,7 @@ namespace BackToBg.Core.Business.Paginators
                     {
                         this.pageNumber--;
                     }
+                    this.lineNumber = 0;
                     break;
                 case ConsoleKey.RightArrow:
                     if (this.pageNumber >= this.PagesCount - 1)
@@ -63,6 +107,19 @@ namespace BackToBg.Core.Business.Paginators
                     {
                         this.pageNumber++;
                     }
+                    this.lineNumber = 0;
+                    break;
+                case ConsoleKey.DownArrow:
+                    if (this.lineNumber < ItemsPerPage - 1 && this.lineNumber + this.pageNumber * ItemsPerPage < this.Items.Count - 1)
+                    {
+                        this.lineNumber++;
+                    }
+                    break;
+                case ConsoleKey.UpArrow:
+                    if (this.lineNumber > 0)
+                    {
+                        this.lineNumber--;
+                    }
                     break;
                 case ConsoleKey.Enter:
                     this.ExecuteAction();
@@ -72,5 +129,7 @@ namespace BackToBg.Core.Business.Paginators
                     break;
             }
         }
+
+        protected abstract string RepresentItem(T item);
     }
 }
--- BACKTOBG/BackToBg.Business/Business/Paginators/QuestsPaginator.cs
+++ BACKTOBG/BackToBg.Business/Business/Paginators/QuestsPaginator.cs
@@ -9,112 +9,30 @@ using BackToBg.Core.Models.Utilities;
 
 namespace BackToBg.Core.Business.Paginators
 {
-    public abstract class QuestsPaginator : Paginator
+    public abstract class QuestsPaginator : Paginator<IQuest>
     {
-        private const int ItemsPerPage = 10;
-
-        private int lineNumber;
         private IEngine engine;
 
-        public QuestsPaginator(IReader reader, IWriter writer, IEngine engine) : base(reader, writer)
+        public QuestsPaginator(IReader reader, IWriter writer, IEngine engine) : base(reader, writer, "Quests")
         {
             this.engine = engine;
-            this.lineNumber = 0;
         }
 
         protected abstract IReadOnlyList<IQuest> Quests { get; }
 
-        protected override int PagesCount
+        protected override IReadOnlyList<IQuest> Items
         {
-            get
-            {
-                var rem = this.Quests.Count % ItemsPerPage != 0 ? 1 : 0;
-                var result = this.Quests.Count / ItemsPerPage + rem;
-
-                return result;
-            }
-        }
-
-        protected override void DisplayPages()
-        {
-            this.writer.Clear();
-            var text = "Quests";
-            this.writer.SetCursorPosition((Constants.ConsoleWidth - text.Length) / 2, Constants.ConsoleHeight / 2 - 1);
-            this.writer.DisplayMessageInColor(text, ConsoleColor.Green);
-            var quests = this.Quests;
-            var questCount = quests.Count;
-            var maxItemsOnCurrentPage = this.pageNumber * (this.PagesCount) + ItemsPerPage;
-            var upperBorder = Math.Min(maxItemsOnCurrentPage, questCount);
-            var i = this.pageNumber * ItemsPerPage;
-            for (; i < upperBorder; i++)
-            {
-                this.writer.SetCursorPosition((Constants.ConsoleWidth - quests[0].Name.Length) / 2, Constants.ConsoleHeight / 2 + i % ItemsPerPage);
-                if (i == this.lineNumber + this.pageNumber * ItemsPerPage)
-                {
-                    this.writer.WriteLine($">{this.Quests[i].Name}<");
-                }
-                else
-                {
-                    this.writer.WriteLine($"{this.Quests[i].Name}");
-                }
-            }
-
-            var message = $"Page {this.pageNumber + 1}/{this.PagesCount}";
-            this.writer.SetCursorPosition((Constants.ConsoleWidth - message.Length) / 2, Constants.ConsoleHeight / 2 + ItemsPerPage + 1);
-            this.writer.DisplayMessageInColor(message, ConsoleColor.Magenta);
-        }
-
-        protected override void ReadKeyInput()
-        {
-            var key = this.reader.ReadKey();
-            switch (key.Key)
-            {
-                case ConsoleKey.LeftArrow:
-                    if (this.pageNumber <= 0)
-                    {
-                        this.pageNumber = this.PagesCount - 1;
-                    }
-                    else
-                    {
-                        this.pageNumber--;
-                    }
-                    this.lineNumber = 0;
-                    break;
-                case ConsoleKey.RightArrow:
-                    if (this.pageNumber >= this.PagesCount - 1)
-                    {
-                        this.pageNumber = 0;
-                    }
-                    else
-                    {
-                        this.pageNumber++;
-                    }
-                    this.lineNumber = 0;
-                    break;
-                case ConsoleKey.DownArrow:
-                    if (this.lineNumber < ItemsPerPage - 1 && this.lineNumber + this.pageNumber * ItemsPerPage < this.Quests.Count - 1)
-                    {
-                        this.lineNumber++;
-                    }
-                    break;
-                case ConsoleKey.UpArrow:
-                    if (this.lineNumber > 0)
-                    {
-                        this.lineNumber--;
-                    }
-                    break;
-                case ConsoleKey.Enter:
-                    this.ExecuteAction();
-                    break;
-                case ConsoleKey.Escape:
-                    this.shouldBeRunning = false;
-                    break;
-            }
+            get => this.Quests;
         }
 
         protected override void ExecuteAction()
         {
-            throw new NotImplementedException("Viewing quest details not yet implemented");
+            throw new NotImplementedException("Viewing item details not yet implemented");
+        }
+
+        protected override string RepresentItem(IQuest item)
+        {
+            return item.Name;
         }
     }
 }
